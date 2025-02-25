0e1cc202773b367c08cc49e32afe0ca9e68cb116
Yuto-komatech <1gk3269y@komazawa-u.ac.jp>
2025-02-25 21:54:31 +0900

    github_pagesにあわせた

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/home/',  // GitHubリポジトリ名を指定
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
